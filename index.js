866eb89a5b71fe6503977f9c2135adb69b4ede06
Sam Gladstone <gladston@umich.edu>
2017-12-03 18:06:34 -0700

    Try out "seen" directive and iterating over array els

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ const app2 = new Vue({
     message: `This page was loaded on/at: ${new Date().toLocaleString()}`
   }
 });
+
+const app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen:true
+  }
+});
+
+const app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { name: 'Learn JavaScript' },
+      { name: 'Learn Vue' },
+      { name: 'Build something worth being proud of' }
+    ]
+  }
+});

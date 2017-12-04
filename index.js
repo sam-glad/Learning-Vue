e3fc06ca0ed3dfa7eed703c4bff492c15aa73fe8
Sam Gladstone <gladston@umich.edu>
2017-12-03 17:56:49 -0700

    Trying v-bind:title

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const app = new Vue({
     message: 'Hello, Vue!'
   }
 });
+
+const app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: `This page was loaded on/at: ${new Date().toLocaleString()}`
+  }
+});
